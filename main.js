8a0e07f3a07e47c701ae9e9f6c4cc0d8de909569
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-07-25 21:30:22 +0200

    adding_function

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ const body = document.querySelector('body');
 document.getElementById('wrap').onload = createDiv()
 
 function createDiv() {
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 256; i++) {
         const div = document.createElement('div');
+        div.id = 'dive';
+        const divStyle = div.style;
+        divStyle.backgroundColor = 'white';
+        divStyle.height = '50px';
+        divStyle.width = '50px';
         document.getElementById('wrap').appendChild(div);
     }
 };
 
+const wrapperStyle = wrap.style;
+wrapperStyle.height = '800px';
+wrapperStyle.width = '800px';
+wrapperStyle.display = 'flex';
+wrapperStyle.flexDirection = 'row';
+wrapperStyle.flexWrap = 'wrap';
 
-const divStyle = document.querySelector('div').style;
-divStyle.backgroundColor = 'black';
-divStyle.height = '100px';
-divStyle.width = '100px';
+let dive = document.getElementById('dive').innerHTML;
+
+dive.onmouseover = function() {paintBrush()};
+
+function paintBrush() {
+    dive.backgroundColor = 'black';
+};
