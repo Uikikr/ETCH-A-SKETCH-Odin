2f4214421e37e53210a8abd79705bced15020cfc
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-07-25 22:52:19 +0200

    changes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById('wrap').onload = createDiv()
 function createDiv() {
     for (i = 0; i < 256; i++) {
         const div = document.createElement('div');
-        div.id = 'dive';
+        div.id = i;
         div.className = 'numOne';
         const divStyle = div.style;
         divStyle.backgroundColor = 'white';
@@ -24,9 +24,11 @@ wrapperStyle.display = 'flex';
 wrapperStyle.flexDirection = 'row';
 wrapperStyle.flexWrap = 'wrap';
 
-let divH = document.getElementById('dive').innerHTML;
+let divH = document.getElementsByClassName('numOne');
 
-divH.addEventListener('click', function (e) {
-    console.log(this.className)           // logs the className of my_element
-    console.log(e.currentTarget === this) // logs `true`
+document.addEventListener('mouseover', function (e) {
+    const div = e.target;
+    if (div.className === 'numOne') {
+        div.style.backgroundColor = 'black'
+    }
   });
