ad5e6aa087807cdf0e9376b129955af45dc9753c
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-08-07 22:37:15 +0200

    solved

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
 const body = document.querySelector('body');
 
+document.getElementById('wrap').onload = createDiv(), createButton()
 
-document.getElementById('wrap').onload = createDiv()
-
-function createDiv() {
-    for (i = 0; i < 256; i++) {
+function createDiv(width = 16) {
+    console.log(width, 50)
+    for (i = 0; i < width*width; i++) {
         const div = document.createElement('div');
         div.id = i;
         div.className = 'numOne';
         const divStyle = div.style;
         divStyle.backgroundColor = 'white';
-        divStyle.height = '25px';
-        divStyle.width = '25px';
+        divStyle.height = 400 / width + 'px';
+        divStyle.width = 400 / width + 'px';
         divStyle.border = '1px, "solid", "black"';
         document.getElementById('wrap').appendChild(div);
     }
+    let wrapperStyle = wrap.style;
+    wrapperStyle.height = '400px';
+    wrapperStyle.width = '400px';
+    wrapperStyle.display = 'flex';
+    wrapperStyle.flexDirection = 'row';
+    wrapperStyle.flexWrap = 'wrap';
 };
 
-const wrapperStyle = wrap.style;
-wrapperStyle.height = '432px';
-wrapperStyle.width = '432px';
-wrapperStyle.display = 'flex';
-wrapperStyle.flexDirection = 'row';
-wrapperStyle.flexWrap = 'wrap';
-
 let divH = document.getElementsByClassName('numOne');
+
+function createButton() {
+    const bottun = document.createElement('button');
+    let t = document.createTextNode('Change');
+    bottun.id = 'button1';
+    document.getElementById('body').appendChild(bottun);
+    document.getElementById('button1').appendChild(t);
+}
+
+function mouseUpp() {
+    let pNumber = Number(window.prompt('Type a number', ' '))
+    console.log(pNumber);
+    document.getElementById('wrap').innerHTML = null;
+    createDiv(pNumber);
+}
 
 document.addEventListener('mouseover', function (e) {
     const div = e.target;
     if (div.className === 'numOne') {
         div.style.backgroundColor = 'black'
     }
-  });
+});
+
+document.getElementById('button1').onmouseup = function () {
+    mouseUpp()
+}
