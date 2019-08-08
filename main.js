cff9cb732172beecea12a78614d263764047d929
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-08-08 10:01:39 +0200

    random_color

--- main.js
+++ main.js
@@ -21,36 +21,54 @@ function createDiv(width = 16) {
     wrapperStyle.display = 'flex';
     wrapperStyle.flexDirection = 'row';
     wrapperStyle.flexWrap = 'wrap';
+    wrapperStyle.border = 'solid';
 };
 
 let divH = document.getElementsByClassName('numOne');
 
 function createButton() {
-    const bottun = document.createElement('button');
-    let t = document.createTextNode('Change');
-    bottun.id = 'button1';
-    document.getElementById('body').appendChild(bottun);
-    document.getElementById('button1').appendChild(t);
+    for (i = 0; i < 2; i++) {
+        let bottun = document.createElement('button');
+        document.getElementById('body').appendChild(bottun);
+        bottun.id = 'button'+i;
+    };
+    //const bottun = document.createElement('button');
+    let t = document.createTextNode('Change size');
+    let t2 = document.createTextNode('Random color')
+    
+    document.getElementById('button0').appendChild(t);
+    document.getElementById('button1').appendChild(t2);
 }
 
 function mouseUpp() {
-    let pNumber = Number(window.prompt('Type a number', ' '))
+    let pNumber = Number(window.prompt('Type the number of squares you want in a row. Please be kind to chrome and keep it under 200', ' '))
     console.log(pNumber);
     document.getElementById('wrap').innerHTML = null;
     createDiv(pNumber);
 }
 
-function mouseOvver(e, color = 'RGB(0, 0, 0)') {
+function mouseOvver(e, color = 'RGB'+'(0, 0, 0)') {
     const div = e.target;
     if (div.className === 'numOne') {
         div.style.backgroundColor = color
     }
 };
 
+function changeColor() {
+    let rColor = 'RGB'+(Math.floor(Math.random()*250), Math.floor(Math.random()*250), Math.floor(Math.random()*250));
+    mouseOvver(rColor);
+    console.log(mouseOvver());
+
+};
+
 document.getElementById('wrap').onmouseover = function(e) {
     mouseOvver(e)
 }
 
-document.getElementById('button1').onmouseup = function() {
+document.getElementById('button0').onmouseup = function() {
     mouseUpp()
+}
+
+document.getElementById('button1').onmouseup = function() {
+    changeColor();
 }
